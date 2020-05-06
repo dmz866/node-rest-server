d49a73937831b1852d6c54fc351b5b9e0f39d638
David Murillo <dmz866@hotmail.com>
2020-05-05 22:36:59 -0500

    usuario udpated

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,12 @@
 process.env.PORT = process.env.PORT || 3000;
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://admin:Q1KASRMbkoSwyrKV@cluster0-awerm.mongodb.net/cafe';
+}
+urlDB = 'mongodb+srv://admin:Q1KASRMbkoSwyrKV@cluster0-awerm.mongodb.net/cafe';
+process.env.URL_DB = urlDB;
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@
 
  app.use(require('./routes/usuario'));
 
- mongoose.connect('mongodb://localhost:27017/cafe', (err, res) => {
+ mongoose.connect(process.env.URL_DB, (err, res) => {
      if (err) {
          throw err;
      }
